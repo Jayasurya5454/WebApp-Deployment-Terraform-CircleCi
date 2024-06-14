08379940f2539a0b3048bf01ba8dbe9b75b7a10f
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2024-06-14 22:38:53 +0530

    commit

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "example_instance" {
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
-              sudo amazon-linux-extras install docker -y
+              sudo yum install -y docker
               sudo service docker start
               sudo usermod -a -G docker ec2-user
               sudo docker run -d -p 80:80 jayasurya5454/quotes
